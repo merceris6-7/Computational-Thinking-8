682639825a493cf2de5940b14174b68a66291179
merceris6-7 <mercerlenihan@seattleacademy.org>
2025-06-06 19:34:01 +0000

    race

--- Projects/p4.py
+++ Projects/p4.py
@@ -19,14 +19,14 @@ def create_sprite(image_filename, x=0, y=0):
 
 # Section 2 - Variables
 # TODO - add starting values for all the variables
-x1 = -100
-y1 = 100
+x1 = -200
+y1 = 60
 x2 = -200
-y2 = 200
-x3 = -300
-y3 = 300
-x4 = -400
-y4 = 400
+y2 = 100
+x3 = -200
+y3 = 160
+x4 = -200
+y4 = 220
 
 
 # Section 3 - Setup
@@ -40,12 +40,12 @@ t4 = create_sprite("fish",x4,y4)
 
 # Section 4 - Racing
 # TODO - set how much each variable changes by and increase the number of repeats to at least 30
-# TODO - explain here which sprites are faster or slower
+# TODO - The 4 sprite is going to be the fastest then the first one then the third one then the second one
 for i in range(3):
-    x1 += 10
+    x1 += 15
     x2 += 10
-    x3 += 10
-    x4 += 10
+    x3 += 5
+    x4 += 20
     t1.goto(x1, y1)
     t2.goto(x2, y2)
     t3.goto(x3, y3)
@@ -58,8 +58,12 @@ for i in range(3):
 # TODO - write another elif for player 3 and player 4
 if x1 >= x2 and x1 >= x3 and x1 >= x4:
     print("player 1 wins!")
-elif
+elif x2>= x1 and x2>= x3 and x2>= x4:
     print("player 2 wins!")
+elif x3>= x1 and x3>= x2 and x3>= x4:
+    print("player 3 wins!")
+elif x4>= x1 and x4>= x3 and x4>= x2:
+    print("player 4 wins!")
 
 
 turtle.exitonclick()
